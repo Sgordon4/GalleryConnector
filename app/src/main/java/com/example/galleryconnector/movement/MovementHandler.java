567eca0be88e8567cad732740beaf73f7c2d546e
SGordon4 <seangordonkh@gmail.com>
2024-08-21 21:54:36 -0500

    Starting movement queue

---
+++ app/src/main/java/com/example/galleryconnector/movement/MovementHandler.java
@@ -0,0 +1,19 @@
+package com.example.galleryconnector.movement;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+
+public class MovementHandler {
+	private List<JSONObject> importQueue;
+	private Map<UUID, JSONArray> movementMap;
+
+	
+
+
+
+
+}
