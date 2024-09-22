bc836aa4e9c68f6537900bff567003745878d12f
SGordon4 <seangordonkh@gmail.com>
2024-09-22 01:41:53 -0500

    looping threads is confusing me

--- app/src/main/java/com/example/galleryconnector/repositories/server/ServerFileObservers.java
+++ app/src/main/java/com/example/galleryconnector/repositories/server/ServerFileObservers.java
@@ -2,17 +2,25 @@ package com.example.galleryconnector.repositories.server;
 
 import com.google.gson.JsonObject;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 public class ServerFileObservers {
 
 	private final List<SFileObservable> listeners;
-	private boolean isListening = false;
+	private final ExecutorService executor;
+	private Future<?> longpoller;
 
 
 	public ServerFileObservers() {
 		listeners = new ArrayList<>();
+		executor = Executors.newSingleThreadExecutor();
 	}
 
 
@@ -32,11 +40,38 @@ public class ServerFileObservers {
 
 
 	//TODO Make a listener thread for longpoll
-	public boolean startListening() {
-		if(isListening) return true;
+	public void startListening(int journalID, UUID accountUID) throws ExecutionException, InterruptedException {
+		//If we're already listening, return
+		if(longpoller != null)
+			return;
+
+		//Start listening
+		longpoller = executor.submit(() -> {
+
+			while (true) {
+
+				try {
+					longpoll(journalID);
+				} catch (IOException e) {
+					throw new RuntimeException(e);
+				}
+
+			}
+
+
+		});
+		longpoller.get();
+	}
 
 
 
+	public void longpoll(int journalID) throws IOException {
+		//Try to get any new journal entries
+		List<JsonObject> entries = ServerRepo.getInstance().longpollJournalEntriesAfter(journalID);
+
+		//If we get any entries back, notify the observers
+		for(JsonObject entry : entries)
+			notifyObservers(entry);
 	}
 
 
